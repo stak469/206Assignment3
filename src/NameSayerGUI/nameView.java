ee43e87d93428e3eefdae9fce75c00594c0ec39c
jake-good <jgoo479@aucklanduni.ac.nz>
2018-09-22 12:14:23 +1200

    Created base for name interface and added some resources

---
+++ src/NameSayerGUI/nameView.java
@@ -0,0 +1,16 @@
+package NameSayerGUI;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class nameView implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    //set up list view here + actions for left and right arrows.
+    }
+
+    public void play() {}
+    public void record() {}
+}
