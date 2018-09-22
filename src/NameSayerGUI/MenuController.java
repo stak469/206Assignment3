3a8740b8a8c27534b9375c85cc6895b0a2bdf1a9
Shunji Takano <stak469@aucklanduni.ac.nz>
2018-09-22 23:49:23 +1200

    Added randomise button and scene chagne on button press

--- src/NameSayerGUI/MenuController.java
+++ src/NameSayerGUI/MenuController.java
@@ -3,11 +3,19 @@ package NameSayerGUI;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
+import javafx.stage.Stage;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
@@ -18,7 +26,13 @@ import java.util.ResourceBundle;
 public class MenuController implements Initializable {
 
     @FXML
-    ListView<String> listView;
+    public ListView<String> listView;
+
+    @FXML
+    public CheckBox randomise;
+
+    public Boolean random = false;
+
 
     List<String> listName = new ArrayList<String>();
 
@@ -48,4 +62,19 @@ public class MenuController implements Initializable {
         listView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
     }
 
+    @FXML
+    public void onRandomise(){
+        random = true;
+    }
+
+    @FXML
+    public void changeScreenToNameView(ActionEvent event) throws IOException {
+        Parent nameViewParent = FXMLLoader.load(getClass().getResource("nameView.fxml"));
+        Scene nameViewScene = new Scene(nameViewParent);
+
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+        window.setScene(nameViewScene);
+        window.show();
+    }
 }
